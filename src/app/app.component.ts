8a5e8bcb27b24db505d2eec5c3b584e30b87e689
sabaleonel <leonel.saba@hotmail.com>
2025-02-24 22:12:54 +0100

    feat: add PlayingCard component with styles and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template : '',
+  imports: [PlayingCardComponent],
+  templateUrl : './app.component.html',
+  styleUrl: './app.component.css',
 })
 
 export class AppComponent {
---
+++ src/app/components/playing-card/playing-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-playing-card',
+  imports: [],
+  templateUrl: './playing-card.component.html',
+  styleUrl: './playing-card.component.css'
+})
+export class PlayingCardComponent {
+
+}
